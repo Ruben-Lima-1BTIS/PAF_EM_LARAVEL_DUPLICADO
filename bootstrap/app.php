38c668ce7647d2dacbd2a1fd43d5afe2c5b4a090
Ruben-Lima-1BTIS <5488@eclisboa.net>
2026-04-18 20:02:39 +0100

    add: implement custom error handling for HTTP exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\MessageBag;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +28,22 @@ return Application::configure(basePath: dirname(__DIR__))
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            return response()->view('errors.405', [
+                'errors' => new MessageBag(),
+            ], 405);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            return response()->view('errors.404', [
+                'errors' => new MessageBag(),
+            ], 404);
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
+            return response()->view('errors.403', [
+                'errors' => new MessageBag(),
+            ], 403);
+        });
+
     })->create();
