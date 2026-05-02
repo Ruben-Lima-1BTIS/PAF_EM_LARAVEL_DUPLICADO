3008f2cbf0bc1776871ffc097075b42f805728b5
Ruben <5488@eclisboa.net>
2026-05-02 22:00:06 +0100

    fix: enhance password security in changePassword method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Rules\StrongPassword;
 
 class UserController extends Controller
 {
     public function changePassword(Request $request)
     {
         $request->validate([
-            'password' => 'required|min:8|confirmed',
+            'password' => ['required', 'string', 'confirmed', new StrongPassword()],
         ]);
 
         auth()->user()->update([
-            'password' => bcrypt($request->password),
+            'password' => Hash::make($request->password),
             'first_login' => 0,
         ]);
 
